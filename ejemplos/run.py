f290657586daced2d769938bfbdbd0b69a58b0bf
SamuelRe17 <116181899+SamuelRe17@users.noreply.github.com>
2023-05-13 14:58:58 -0500

    Update run.py

--- ejemplos/run.py
+++ ejemplos/run.py
@@ -5,14 +5,40 @@
 
 # objeto 01
 
-# crear
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+    
+    def __str__(self):
+        return f"Mi nombre es {self.nombre} y tengo {self.edad} años."
 
-# Presentar objeto; usar el método __st__
+
+persona1 = Persona("Juan", 30)
+
+print(persona1)
+
 
 # objeto 02
 
-# crear ingresando valores por teclado
+class Estudiante:
+    def __init__(self, nombre, edad, carrera):
+        self.nombre = nombre
+        self.edad = edad
+        self.carrera = carrera
+    
+    def __str__(self):
+        return f"Mi nombre es {self.nombre}, tengo {self.edad} años y estudio {self.carrera}."
 
-# Presentar objeto; usar el método __st__
+
+nombre = input("Ingrese el nombre del estudiante: ")
+edad = int(input("Ingrese la edad del estudiante: "))
+carrera = input("Ingrese la carrera del estudiante: ")
+
+
+estudiante1 = Estudiante(nombre, edad, carrera)
+
+
+print(estudiante1)
 
 
