da8335cfde4ebffa3dc7ab04904659b8ead8a3c0
SamuelRe17 <116181899+SamuelRe17@users.noreply.github.com>
2023-05-13 15:01:39 -0500

    Update run2.py

--- ejemplos/run2.py
+++ ejemplos/run2.py
@@ -4,15 +4,53 @@
 # Crear dos objetos de la clase 02
 
 # objeto 01
+import math
 
-# crear
+class Circulo:
+    def __init__(self, radio):
+        self.radio = radio
+    
+    def calcular_area(self):
+        return math.pi * self.radio ** 2
+    
+    def calcular_perimetro(self):
+        return 2 * math.pi * self.radio
+    
+    def __str__(self):
+        return f"El círculo tiene un radio de {self.radio}, un área de {self.calcular_area():.2f} y un perímetro de {self.calcular_perimetro():.2f}."
 
-# Presentar objeto; usar el método __st__
+radio = float(input("Ingrese el radio del círculo: "))
+
+
+circulo1 = Circulo(radio)
+
+
+print(circulo1)
+
 
 # objeto 02
+class Rectangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+    
+    def calcular_area(self):
+        return self.base * self.altura
+    
+    def calcular_perimetro(self):
+        return 2 * (self.base + self.altura)
+    
+    def __str__(self):
+        return f"El rectángulo tiene una base de {self.base}, una altura de {self.altura}, un área de {self.calcular_area()} y un perímetro de {self.calcular_perimetro()}."
 
-# crear ingresando valores por teclado
 
-# Presentar objeto; usar el método __st__
+base = float(input("Ingrese la base del rectángulo: "))
+altura = float(input("Ingrese la altura del rectángulo: "))
+
+
+rectangulo1 = Rectangulo(base, altura)
+
+
+print(rectangulo1)
 
 
